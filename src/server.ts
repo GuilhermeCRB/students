d5a78963e65090e365a78d0eb11850bab1dbe93d
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-08-09 19:34:24 -0300

    refac: test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 })
